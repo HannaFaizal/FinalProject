f8bcbd870799d625118ad9776bd70d93bb427a5f
rishnywatsana <rishny20@gmail.com>
2023-11-17 15:03:38 +0530

    RishnyfirstCommit

--- src/main/java/com/demo/ClinicalManagementApplication.java
+++ src/main/java/com/demo/ClinicalManagementApplication.java
@@ -5,7 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class ClinicalManagementApplication {
-
+	
 	public static void main(String[] args) {
 		SpringApplication.run(ClinicalManagementApplication.class, args);
 	}
