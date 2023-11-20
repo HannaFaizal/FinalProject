ef8d5d14d2143b78483b9320925e63fbd7916144
SarathSujith <sarathsujith2000@gmail.com>
2023-11-20 12:09:10 +0530

    Sarath second commit

--- src/main/java/com/demo/repository/ITestListRepository.java
+++ src/main/java/com/demo/repository/ITestListRepository.java
@@ -1,10 +1,6 @@
 package com.demo.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
-<<<<<<< HEAD
-
-=======
->>>>>>> branch 'master' of https://github.com/HannaFaizal/FinalProject.git
 import com.demo.entity.TestList;
 
 public interface ITestListRepository extends JpaRepository<TestList, Integer> {
--- src/main/java/com/demo/repository/ITestReportRepository.java
+++ src/main/java/com/demo/repository/ITestReportRepository.java
@@ -3,10 +3,8 @@ package com.demo.repository;
 import org.springframework.data.jpa.repository.JpaRepository;
 import com.demo.entity.TestReport;
 
-<<<<<<< HEAD
+
 public interface ITestReportRepository extends JpaRepository<TestReport, Integer>{
-=======
-public interface ITestReportRepository extends JpaRepository<TestReport, Integer> {
->>>>>>> branch 'master' of https://github.com/HannaFaizal/FinalProject.git
+
 
 }
